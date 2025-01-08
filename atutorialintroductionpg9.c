090ecdea31cce3084156934e118da1501060233e
Jno, M <rdane9885@gmail.com>
2025-01-08 14:33:55 -0500

    Create atutorialintroductionpg9.c
     The first program to write is the same for all languages: 
    Print the words: Hello, World!

---
+++ atutorialintroductionpg9.c
@@ -0,0 +1,3 @@
+// EXERCISE 1.1 A Tutorial Introduction pg.9
+#include <stdio.h> // Library for standard input and output 
+printf("Hello, World!\n"); // Using the 'printf()' command from the input/output library to print a string
